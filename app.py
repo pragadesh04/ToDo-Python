fec96a85cbe2ef59affcbe3fa792c0701fe4ba12
pragadesh04 <pragaofficial004@gmail.com>
2024-09-01 16:56:13 +0530

    added user authentication

--- app.py
+++ app.py
@@ -1,98 +1,139 @@
+import bcrypt as bc
 from flask_pymongo import PyMongo
 from flask import Flask, session, render_template, request, url_for, redirect
 from bson import ObjectId
 import os
-
-
-print(str(os.getcwd()))
+import re
+import secrets
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://pragadeshr04:<Password>@user-datas.g2ls6.mongodb.net/Users?retryWrites=true&w=majority"
+def is_valid_password(password):
+    if len(password) < 8:
+        return False, "Password must be at least 8 characters long"
+    if not re.search(r"[A-Z]", password):
+        return False, "Password must contain at least one uppercase letter"
+    if not re.search(r"[a-z]", password):
+        return False, "Password must contain at least one lowercase letter"
+    if not re.search(r"[0-9]", password):
+        return False, "Password must contain at least one digit"
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        return False, "Password must contain at least one special character"
+    return True, ""
+
+# Configurations
+app.config["MONGO_URI"] = "mongodb+srv://pragadeshr04:<password>@user-datas.g2ls6.mongodb.net/Users?retryWrites=true&w=majority"
+app.config["SECRET_KEY"] = secrets.token_hex(32)
+
 mongo = PyMongo(app)
-print(mongo)
-
 db = mongo.db
-collection = db.ToDo
-no = mongo.db.ToDo
-
+notes = db.ToDo
+users = db.usersToDo
 
 @app.route("/")
-@app.route("/home")
-def home():
+@app.route("/home/<name>")
+def home(name):
+    if not session.get("username"):
+        return render_template("login.html")
     
-    datas = list(no.find())
+    datas = list(notes.find())
+    print(f"{session.get('username')}".center(150, "="))
     if datas is not None:
-        return render_template("notes.html ", datas = datas)
-    else:
-        return render_template("notes.html")
+        return render_template("notes.html", datas=datas)
 
-@app.route("/edited/<id>", methods = ['POST', 'GET'])
+@app.route("/edited/<id>", methods=['POST', 'GET'])
 def edit(id):
     title = request.form.get("editedtitle")
     desc = request.form.get("editeddesc")
+    query = {"_id": ObjectId(id)}
     
-    query = {"_id" : ObjectId(id)}
+    update_data = {}
+    if title:
+        update_data["title"] = title
+    if desc:
+        update_data["desc"] = desc
     
-    if desc == "":
-        no.update_one(query, {"$set":{"title":title}})
-    elif title == "":
-        no.update_one(query, {"$set":{"desc": desc}})
-    else:
-        no.update_one(query, {"$set":{"title":title, "desc":desc}})
-        
-    print(query)
+    if update_data:
+        notes.update_one(query, {"$set": update_data})
     
-    datas = list(no.find())
-    return render_template("notes.html", datas = datas)
-    
+    datas = list(notes.find())
+    return render_template("notes.html", datas=datas)
 
-@app.route("/form", methods = ['POST', 'GET'])
+@app.route("/form", methods=['POST', 'GET'])
 def form():
     try:
         title = request.form.get("title")
         desc = request.form.get("desc")
-        # print(title,desc)
-                
-        no.insert_one({"title":title, "desc":desc})
-        
-        datas = list(no.find())
-        print(datas[0])
-        
-        # return "<h2>Okay</h2>"
-        return render_template("notes.html", datas = datas)
+        notes.insert_one({"title": title, "desc": desc})
+        datas = list(notes.find())
+        return render_template("notes.html", datas=datas)
     except Exception as e:
         print(e)
-        return "e"
-        
+        return "Error"
+
 @app.route("/delete/<id>")
 def delete(id):
-    print()
-    print(id)
-    print()
-     
-    id_ = ObjectId(id)
-    print(id_)
-    
-    query={"_id":id_}
-    no.delete_one(query)
-    
+    query = {"_id": ObjectId(id)}
+    notes.delete_one(query)
     return redirect(url_for('home'))
 
-@app.route("/signup", methods = ['POST', 'GET'])
+@app.route("/signup")
 def signup():
-    name = request.form.get("username")
-    password = request.form.get("password")
-    print(name, password)
     return render_template('signup.html')
 
-# @app.route("/signin")
-@app.route("/signin", methods = ['POST', 'GET'])
-def signin():
+@app.route("/signup-form", methods=['POST', 'GET'])
+def signup_form():
     name = request.form.get("username")
     password = request.form.get("password")
-    print(name, password)
+    newpassword = request.form.get("newpassword")
+    mail = request.form.get("mail")
+    
+    msg = is_valid_password(password)
+    
+    if msg[0] is False:
+        return render_template("message.html", message  = msg[1], message_head = "Not valid Info")
+    if password != newpassword:
+        return render_template("message.html", message  = "Password Does Not Match", message_head = "Not valid Info")
+    
+    userN = users.find_one({"username": name})
+    userM = users.find_one({"mail": mail})
+    
+    if userN or userM:
+        return redirect(url_for("signin"))
+    
+    salt = bc.gensalt()
+    hashPass = bc.hashpw(password.encode('utf-8'), salt)
+    users.insert_one({"uname": name, "password": hashPass, "mail": mail})
     return render_template('login.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('login.html')
+
+@app.route("/signin-form", methods=['POST', 'GET'])
+def signin_form():
+    name = request.form.get("username")
+    password = request.form.get("password")
+    
+    userN = users.find_one({"uname": name})
+    
+    if not userN:
+        return redirect(url_for("signup"))
+    
+    if bc.checkpw(password.encode('utf-8'), userN["password"]):
+        session['username'] = name
+        return redirect(url_for("home", name  = name))
+    else:
+        return redirect(url_for("signin"))
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for("home"))
+
+@app.route("/message")
+def message():
+    return render_template("message.html", message_head = "empty Message", message = "Know Better Get Better")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
